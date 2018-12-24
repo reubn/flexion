7652cc3ba9df9bbe4efa2304d86ef61043465b8a
Reuben <reuben.eggar@me.com>
2018-12-24 10:39:44 +0000

    ✨ Log Time

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ process.stdin.on('readable', () => {
   process.stdin.resume()
   var chunk = process.stdin.read();
   if (chunk !== null) {
-    const result = conjugate(chunk.trim())
+    const trimmed = chunk.trim()
+
+    const start = process.hrtime()
+    const result = conjugate(trimmed)
+    const end = process.hrtime(start)
+
     console.log(util.inspect(JSON.parse(JSON.stringify(result)), {
       depth: Infinity,
       compact: false,
       colors: true
     }))
+    console.log(end[0], 's', end[1] / 10**6, 'ms')
   }
 });
 
