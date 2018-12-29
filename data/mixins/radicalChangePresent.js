d5d7e8547343fb6e4369fd8a01732484dfde2e34
Reuben <reuben.eggar@me.com>
2018-12-29 23:17:18 +0000

    ✨ Revert to 'this'

--- data/mixins/radicalChangePresent.js
+++ data/mixins/radicalChangePresent.js
@@ -1,17 +1,17 @@
 import conjugate from '../../conjugate'
 import replaceLast from '../../util/replaceLast'
 
-export default (from, to) => function radicalChangePresentMixin(verb){
+export default (from, to) => function radicalChangePresentMixin(){
   const {conjugation: {indicative:{present: endings}}} = conjugate(this.infinitive.slice(-2))
 
   return {
     singular: {
-      first(){return replaceLast(verb.root(), from, to) + endings.singular.first()},
-      second(){return replaceLast(verb.root(), from, to) + endings.singular.second()},
-      third(){return replaceLast(verb.root(), from, to) + endings.singular.third()}
+      first(){return replaceLast(this.root(), from, to) + endings.singular.first()},
+      second(){return replaceLast(this.root(), from, to) + endings.singular.second()},
+      third(){return replaceLast(this.root(), from, to) + endings.singular.third()}
     },
     plural: {
-      third(){return replaceLast(verb.root(), from, to) + endings.plural.third()}
+      third(){return replaceLast(this.root(), from, to) + endings.plural.third()}
     }
   }
 
