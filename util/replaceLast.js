66c4d0f3ab26d6480765b99e8eed3f043e4c4373
Reuben <reuben.eggar@me.com>
2018-12-30 13:17:47 +0000

    🐛

--- util/replaceLast.js
+++ util/replaceLast.js
@@ -1,4 +1,6 @@
 export default (haystack, needle, replacement) => {
   const lastIndex = haystack.lastIndexOf(needle)
+  
+  if(lastIndex === -1) return haystack
   return haystack.slice(0, lastIndex) + replacement + haystack.slice(lastIndex + needle.length, haystack.length)
 }
