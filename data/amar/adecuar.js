4bdc33774c541434f94a074b53e020e389aa597b
Reuben <reuben.eggar@me.com>
2018-12-30 13:20:27 +0000

    ✨ Handle 2 options for adecuar Subjunctive

--- data/amar/adecuar.js
+++ data/amar/adecuar.js
@@ -1,12 +1,25 @@
-import radicalChangePresentMixin from '../mixins/radicalChangePresent'
+import mergeAndOverlapIntoArrays from '../../util/mergeAndOverlapIntoArrays'
 
+import radicalChangePresentMixin from '../mixins/radicalChangePresent'
+import maintainSoundSubjunctiveMixin from '../mixins/maintainSoundSubjunctive'
+import passInMixin from '../mixins/passIn'
 
 export default {
   verb: 'adecuar',
   test: i => ['adecuar', 'apropincuar', 'colicuar', 'evacuar', 'licuar', 'promiscuar'].includes(i),
   inflections: {
     indicative: {
-      present: radicalChangePresentMixin('u', 'ú')
+      present: passInMixin(verb => mergeAndOverlapIntoArrays(verb.indicative.present, radicalChangePresentMixin('u', 'ú').call(verb, verb)))
+    },
+    subjunctive: {
+      present: passInMixin(
+        verb => mergeAndOverlapIntoArrays(
+          verb.subjunctive.present,
+          maintainSoundSubjunctiveMixin('u', 'ú', {
+            pluralFirstAndSecondSkip: true
+          }).call(verb, verb)
+        )
+      )
     }
   }
 }
