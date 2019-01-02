a60ab7cd10fed9e31fd531898a4bef06d1546194
Reuben <reuben.eggar@me.com>
2019-01-02 22:57:29 +0000

    🐛

--- data/temer/hacer.js
+++ data/temer/hacer.js
@@ -11,18 +11,18 @@ import mergeMixin from '../mixins/merge'
 
 export default {
   verb: 'hacer',
-  test: i => i.endsWith('hacer'),
+  test: i => i.endsWith('hacer') || i === 'satisfacer',
   inflections: {
-    participle(){return replaceLast(this.root(), 'hac', 'hecho')},
+    participle(){return replaceLast(this.root(), 'ac', 'echo')},
     indicative: {
       present: {
         singular: {
-          first(){return replaceLast(this.root(), 'hac', 'hago')}
+          first(){return replaceLast(this.root(), 'ac', 'ago')}
         }
       },
-      preterite: mergeMixin(pretéritoGraveMixin(verb => replaceLast(verb.root(), 'hac', 'hic')), verb => {
-        const {singular: {first}} = verb.infinitive === 'rehacer' ? pretéritoGraveMixin(verb => replaceLast(verb.root(), 'hac', 'híc')).call(verb, verb) : {singular:{}}
-        const {singular: {third}} = pretéritoGraveMixin(verb => replaceLast(verb.root(), 'hac', verb.infinitive === 'rehacer' ? 'híz' : 'hiz')).call(verb, verb)
+      preterite: mergeMixin(pretéritoGraveMixin(verb => replaceLast(verb.root(), 'ac', 'ic')), verb => {
+        const {singular: {first}} = verb.infinitive === 'rehacer' ? pretéritoGraveMixin(verb => replaceLast(verb.root(), 'ac', 'íc')).call(verb, verb) : {singular:{}}
+        const {singular: {third}} = pretéritoGraveMixin(verb => replaceLast(verb.root(), 'ac', verb.infinitive === 'rehacer' ? 'íz' : 'iz')).call(verb, verb)
         return {
           singular: {
             first,
@@ -30,11 +30,11 @@ export default {
           }
         }
     }),
-      future: irregularFutureMixin(verb => replaceLast(verb.root(), 'hac', 'har')),
-      conditional: irregularConditionalMixin(verb => replaceLast(verb.root(), 'hac', 'har'))
+      future: irregularFutureMixin(verb => replaceLast(verb.root(), 'ac', 'ar')),
+      conditional: irregularConditionalMixin(verb => replaceLast(verb.root(), 'ac', 'ar'))
     },
     subjunctive: {
-      present: radicalChangeSubjunctiveAllMixin('hac', 'hag')
+      present: radicalChangeSubjunctiveAllMixin('ac', 'ag')
     }
   }
 }
