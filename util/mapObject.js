03ca2681d132ee261a01bf67a9c54593c1006235
Reuben <reuben.eggar@me.com>
2019-01-02 20:18:16 +0000

    ✨ Pass All Keys

--- util/mapObject.js
+++ util/mapObject.js
@@ -1,7 +1,7 @@
-const mapObject = (obj, fn) => {
+const mapObject = (obj, fn, path=[]) => {
   Object.keys(obj).forEach(key => {
-    if(typeof obj[key] === 'object') obj[key] = mapObject(obj[key], fn)
-    else obj[key] = fn({key, value: obj[key]})
+    if(typeof obj[key] === 'object') obj[key] = mapObject(obj[key], fn, [...path, key])
+    else obj[key] = fn({keys: [...path, key], value: obj[key]})
   })
   return obj
 }
