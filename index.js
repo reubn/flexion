a181c3c6770733d85d3cda1537ab569c6ae384f1
Reuben <reuben.eggar@me.com>
2019-01-02 11:33:31 +0000

    ✨ Better Not Found Handling

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const flexion = (internal, request) => {
   const trimmed = request.trim()
   const infinitive = trimmed.endsWith('se') ? trimmed.slice(0, -2) : trimmed
 
-  const result = processTree(infinitive)
+  const result = processTree(infinitive, !internal)
 
+  if(!result) return null
   return internal ? result : JSON.parse(JSON.stringify(result, (key, value) => typeof value === 'function' ? value() : value))
 }
 
